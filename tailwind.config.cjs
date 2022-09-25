6399b629514d3ad142c4180f14cd560b8a101506
Giovanni Lopes <giovanniclopes@gmail.com>
2022-09-24 23:25:37 -0300

    game blocks added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,9 +6,14 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        
+      },
       backgroundImage: {
         galaxy: "url('/background-galaxy.png')",
-      }
+        'nwl-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 40.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
     },
   },
   plugins: [],
